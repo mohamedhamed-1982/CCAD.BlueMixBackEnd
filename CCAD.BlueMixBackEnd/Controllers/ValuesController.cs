91bf4a663c45c054c706be268c094215ba12c6d1
mohamed AbouElElla Hamed <mohhamed@eg.ibm.com>
2017-06-20 17:17:47 +0200

    changed value

--- CCAD.BlueMixBackEnd/Controllers/ValuesController.cs
+++ CCAD.BlueMixBackEnd/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace CCAD.BlueMixBackEnd.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "You entered:"+id;
         }
 
         // POST api/values
